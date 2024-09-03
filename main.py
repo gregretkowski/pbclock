52fd3dfecd2b066ce34c1e448038a08765b525d8
Greg Retkowski <greg@rage.net>
2024-09-03 14:59:18 -0700

    got tides, sunset, surf, etc

--- main.py
+++ main.py
@@ -15,26 +15,13 @@ class MainWindow(QWidget):
         super().__init__()
         self.initUI()
 
-    def initUI(self):
-        self.setGeometry(100, 100, 300, 200)
-        self.setWindowTitle("PyQt5 Grid")
 
-        grid_layout = QGridLayout()
-        self.setLayout(grid_layout)
-
-        cell_texts = [
-            "1 ^v", "2 ^v", "3 ^v",
-            "4 ^v", "5 ^v", "6 ^v"
-        ]
-
-        positions = [(i, j) for i in range(2) for j in range(3)]
-
-    def update_cell(self, grid_layout, position, text):
+    def update_cell(self, grid_layout, position, title, text):
         # Remove existing widget at the position if any
         if grid_layout.itemAtPosition(*position):
             existing_widget = grid_layout.itemAtPosition(*position).widget()
             existing_widget.setParent(None)
-        label = QLabel(text, self)
+        label = QLabel(title+"\n"+text, self)
         grid_layout.addWidget(label, *position)
 
     def initUI(self):
@@ -44,6 +31,13 @@ class MainWindow(QWidget):
         grid_layout = QGridLayout()
         self.setLayout(grid_layout)
 
+        titles = [
+            "Launches", "Surf", "Sunrise/set"
+        ]
+
+        for i, title in enumerate(titles):
+            self.update_cell(grid_layout, (0, i), title, "")
+
         cell_texts = [
             "1 ^v", "2 ^v", "3 ^v",
             "4 ^v", "5 ^v", "6 ^v"
@@ -52,7 +46,7 @@ class MainWindow(QWidget):
         positions = [(i, j) for i in range(2) for j in range(3)]
 
         for position, text in zip(positions, cell_texts):
-            self.update_cell(grid_layout, position, text)
+            self.update_cell(grid_layout, position,"", text)
 
     def fetch_launches(self):
         url = 'https://nextspaceflight.com/launches/nsf_launches/10/'
@@ -85,6 +79,27 @@ class MainWindow(QWidget):
         return {
             'surf_forecast': surf_forecast_formatted
         }
+
+    def fetch_tide(self):
+        url = "https://api.tidesandcurrents.noaa.gov/api/prod//datagetter?&station=9410230&range=1&units=english&datum=MLLW&product=water_level&time_zone=LST_LDT&format=json&application=NOS.COOPS.TAC.COOPSMAP"
+        response = requests.get(url)
+        data = response.json()
+
+        if 'data' in data and len(data['data']) >= 2:
+            last_two_values = data['data'][-2:]
+            last_value = float(last_two_values[-1]['v'])
+            second_last_value = float(last_two_values[-2]['v'])
+
+            trend = "rising" if last_value > second_last_value else "falling"
+            return {
+                'value': last_value,
+                'trend': trend
+            }
+        else:
+            return {
+                'value': 'N/A',
+                'trend': 'N/A'
+            }
 
     def fetch_sunriseset(self):
         from astral import LocationInfo
@@ -121,11 +136,23 @@ class MainWindow(QWidget):
             launch_text = f"{next_launch['name']}\n{next_launch['time_diff']}"
         else:
             launch_text = 'None'
-        self.update_cell(grid_layout, (0, 0), launch_text)
+        self.update_cell(grid_layout, (0, 0), 'Launches', launch_text)
 
         surf_data = self.fetch_surf()
         surf_text = surf_data['surf_forecast']
-        self.update_cell(grid_layout, (0, 1), surf_text)
+        self.update_cell(grid_layout, (0, 1), 'Surf', surf_text)
+
+        sun_data = self.fetch_sunriseset()
+        event_symbol = '^' if sun_data['event'] == 'sunrise' else 'v'
+        sun_time = sun_data['time'].split()[1][:5]  # Extract HH:MM part
+        sun_text = f"{sun_time} {event_symbol}"
+        self.update_cell(grid_layout, (0, 2), 'Sunrise/set', sun_text)
+
+        tide_data = self.fetch_tide()
+        tide_value = tide_data['value']
+        tide_trend = tide_data['trend']
+        tide_text = f"{tide_value:.1f}Ft {'v' if tide_trend == 'falling' else '^'}"
+        self.update_cell(grid_layout, (1, 0), 'Tide', tide_text)
 
 if __name__ == '__main__':
 
@@ -133,10 +160,10 @@ if __name__ == '__main__':
     print(os.getppid())
     app = QApplication(sys.argv)
     main_window = MainWindow()
-    print(main_window.fetch_sunriseset())
-    sys.exit(0)
-    print(main_window.fetch_launches())
-    print(main_window.fetch_surf())
+    #print(main_window.fetch_tide())
+    #sys.exit(0)
+    #print(main_window.fetch_launches())
+    #print(main_window.fetch_surf())
     print('showing main window')
     main_window.show()
     QTimer.singleShot(5000, main_window.update_data)
