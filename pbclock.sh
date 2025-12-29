f62830f5aaa701745a2574500cb1a2a91b52ae7a
Greg Retkowski <greg@rage.net>
2025-12-29 11:42:08 -0800

    set up venv if not present

--- pbclock.sh
+++ pbclock.sh
@@ -1,7 +1,15 @@
 cd /home/pi/pbclock
 git fetch --all
 git reset --hard origin/master
+if [ ! -d "venv" ]; then
+    echo "venv directory not found, creating virtual environment..."
+    python3 -m venv venv
+fi
 . venv/bin/activate
+if [ -f requirements.txt ]; then
+    pip install -r requirements.txt
+fi
+
 python3 main.py &
 sleep 30
 wmctrl -r :ACTIVE: -e 0,0,0,480,320
